c9e572bf200c34ed4b117ad82dfc6a554d4d43fd
RedJohn260 <damir.k93@gmail.com>
2022-05-13 20:52:48 +0200

    added YWPF script

---
+++ Youtube Watch Page Fix/README.md
@@ -0,0 +1,3 @@
+# Youtube Watch Page Fix
+Removes some of the changes to the youtube watch page that were implemented on May 9, 2022.
+Based on Lightbeam24's style for Stylus: https://userstyles.world/style/4623/youtube-watch-page-fix.
---
+++ Youtube Watch Page Fix/Watch Page Fix.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         Youtube Watch Page Fix
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Removes some of the changes to the youtube watch page that were implemented on May 9, 2022.Based on Lightbeam24's style for Stylus: https://userstyles.world/style/4623/youtube-watch-page-fix.
+// @author       RedJohn260, Lightbeam24
+// @match        https://www.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @require  https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
+// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @grant    GM_addStyle
+// @grant    GM.getValue
+// @run-at   document-start
+// ==/UserScript==
+(function() {
+    'use strict';
+    GM_addStyle ( `
+    .ytd-watch-metadata  {
+        display: none !important;
+    }
+    #meta-contents[hidden], #info-contents[hidden] {
+         display: block !important;
+    }
+    #comment-teaser.ytd-watch-metadata {
+         display: none;
+   }
+   #owner.ytd-watch-metadata {
+         border: none;
+   }
+   ytd-watch-metadata:not([modern-metapanel]) #owner.ytd-watch-metadata {
+        border: none;
+  }
+  ytd-watch-metadata[smaller-yt-sans-light-title] h1.ytd-watch-metadata {
+        font-family: "Roboto",sans-serif;
+        font-weight: 400;
+        font-size: 18px;
+  }
+  ytd-video-primary-info-renderer[use-yt-sans20-light] .title.ytd-video-primary-info-renderer {
+        font-family: "Roboto",sans-serif;
+        font-weight: 400;
+        font-size: 18px;
+  }
+  .yt-formatted-string[style-target="bold"] {
+        font-weight: 400;
+  }
+` );
+})();
