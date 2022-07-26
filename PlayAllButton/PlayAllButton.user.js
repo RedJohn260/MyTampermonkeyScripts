ed0453adb40f0669baba5aadf092e9043de565b6
RedJohn260 <damir.k93@gmail.com>
2022-07-26 03:23:50 +0200

    added another matching site to run at

--- PlayAllButton/PlayAllButton.user.js
+++ PlayAllButton/PlayAllButton.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Play All Button
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      1.0.2
 // @description  This script returns "Play All" button for "Uploads" videos when on channel "Videos" tab.Site refresh (F5) required once on "Videos" channel tab. 
 // @author       RedJohn260
 // @homepage     https://github.com/RedJohn260/MyTampermonkeyScripts
 // @downloadURL  https://github.com/RedJohn260/MyTampermonkeyScripts/raw/main/PlayAllButton/PlayAllButton.user.js
 // @updateURL    https://github.com/RedJohn260/MyTampermonkeyScripts/raw/main/PlayAllButton/PlayAllButton.user.js
 // @match        ://*.youtube.com/c/*/videos
+// @match        ://*.youtube.com/channel/*/videos
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
